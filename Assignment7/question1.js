3e148463b4b4afd279c6656a7c1d488f4567b200
AlgorithmChopda <vinaychopda4@gmail.com>
2024-02-19 12:48:27 +0530

    fix[Assignment 7]: question 1 - removed reference

--- Assignment7/question1.js
+++ Assignment7/question1.js
@@ -14,32 +14,13 @@
 
 /*
      Asnwer:
-        creating a copy using "=" will create a shallow copy of the object, i.e internally it points to same object
+        - creating a copy using "=" will just copy reference of the object, i.e internally it points to same object
         so updating one will update the actual object
 
-        so deep copy of object should be created to in order to only update the copy
+        - creating shallow copy just copies elements upto 1 level i.e addres object internally will have reference of the 
+        actual object 
+        - so deep copy of object should be created to in order to only update the copy
 */
-
-// shallow copy
-console.log("Shallow Copy");
-let user = {
-  name: "Harry Potter",
-  age: 12,
-  address: {
-    details: ["4", "Privet Drive"],
-    area: "Little Whinging",
-    city: "Surrey",
-    state: "England",
-  },
-};
-
-console.log("User :", user, "\n");
-
-let userCopy = user;
-userCopy.address.details[0] = "5";
-console.log("After Updation");
-console.log("User :", user, "\n");
-console.log("Copy User :", userCopy);
 
 // deep copy
 console.log("\nDeepCopy");
