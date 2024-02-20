4170af943d6c9a4ccdef9420f457d772047c6779
AlgorithmChopda <vinaychopda4@gmail.com>
2024-02-20 13:00:33 +0530

    feat[Assignment 8]: question 4

---
+++ Assignment8/question4.js
@@ -0,0 +1,20 @@
+// 4.  What will be printed to the console?
+
+const testAsyncFunction = () => {
+  return Promise.reject("Test static reject");
+};
+
+testAsyncFunction()
+  .then((res) => {
+    console.log("Response in then block", res);
+  })
+  .catch((err) => console.log("Error in catch block", err));
+
+/*
+    Answer:
+    
+    Promise is always rejected
+    State: Rejected
+        - promise is rejected the ".catch" run
+        output: Error in catch block Test static reject
+*/
