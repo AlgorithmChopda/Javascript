5e686eb1f1c6d43e369d144219e84aacc9a91c1f
AlgorithmChopda <vinaychopda4@gmail.com>
2024-02-20 14:46:41 +0530

    feat[Assignment 8]: added question for question 6

--- Assignment8/question6.js
+++ Assignment8/question6.js
@@ -1,3 +1,7 @@
+/* 
+  6. Create a promise that makes a fetch call, but resolves with the data only 2 seconds after the data has been received in    the fetch.
+*/
+
 function fetchData() {
   return new Promise((res) => {
     fetch("https://reqres.in/api/users")
