a67f23921704718e413f9f320064898640ba3e72
AlgorithmChopda <vinaychopda4@gmail.com>
2024-02-27 15:13:59 +0530

    feat: added output to all cases

--- Assignment5/question1.js
+++ Assignment5/question1.js
@@ -57,3 +57,77 @@ console.log("JSON:", JSON.stringify(user));
     Actual Output:
     JSON: {"subjects":["C++","Java"],"list":{"name":"abc","age":20},"val":"value","marks":null,"a":"100","isValid":true,"age":50}
 */
+
+let invalidJsonString = "{";
+let parsed = JSON.parse(invalidJsonString);
+console.log("parsed :", parsed);
+
+/*
+    invalid json output:
+    {
+        SyntaxError: Expected property name or '}' in JSON at position 1 (line 1 column 2)
+            at JSON.parse (<anonymous>)
+            at Object.<anonymous> (/media/vinay/New Volume/Josh-Training/Javascript/Assignment5/question1.js:62:19)
+            at Module._compile (node:internal/modules/cjs/loader:1378:14)
+            at Module._extensions..js (node:internal/modules/cjs/loader:1437:10)
+            at Module.load (node:internal/modules/cjs/loader:1212:32)
+            at Module._load (node:internal/modules/cjs/loader:1028:12)
+            at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:142:12)
+            at node:internal/main/run_main_module:28:49
+
+        Node.js v21.6.2
+*/
+
+invalidJsonString = `{
+        name: "John",
+        age: 30,
+        city: "New York"
+}`;
+parsed = JSON.parse(invalidJsonString);
+console.log("parsed :", parsed);
+
+/*
+    invalid json output:
+    undefined:2
+        name: "John",
+        ^
+
+    SyntaxError: Expected property name or '}' in JSON at position 10 (line 2 column 9)
+        at JSON.parse (<anonymous>)
+        at Object.<anonymous> (/media/vinay/New Volume/Josh-Training/Javascript/Assignment5/question1.js:86:19)
+        at Module._compile (node:internal/modules/cjs/loader:1378:14)
+        at Module._extensions..js (node:internal/modules/cjs/loader:1437:10)
+        at Module.load (node:internal/modules/cjs/loader:1212:32)
+        at Module._load (node:internal/modules/cjs/loader:1028:12)
+        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:142:12)
+        at node:internal/main/run_main_module:28:49
+
+    Node.js v21.6.2
+*/
+
+invalidJsonString = `{
+    "name": "Alice",
+    "age": 25,
+    "city": "London",
+}`;
+parsed = JSON.parse(invalidJsonString);
+console.log("parsed :", parsed);
+
+/*
+    invalid json output:
+    undefined:5
+    }
+    ^
+
+    SyntaxError: Expected double-quoted property name in JSON at position 60 (line 5 column 1)
+        at JSON.parse (<anonymous>)
+        at Object.<anonymous> (/media/vinay/New Volume/Josh-Training/Javascript/Assignment5/question1.js:113:19)
+        at Module._compile (node:internal/modules/cjs/loader:1378:14)
+        at Module._extensions..js (node:internal/modules/cjs/loader:1437:10)
+        at Module.load (node:internal/modules/cjs/loader:1212:32)
+        at Module._load (node:internal/modules/cjs/loader:1028:12)
+        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:142:12)
+        at node:internal/main/run_main_module:28:49
+
+    Node.js v21.6.2
+*/
