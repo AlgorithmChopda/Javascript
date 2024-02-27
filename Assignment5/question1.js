c306fb3d6cfffac02ca5a43af9eb89dc1ec5e49c
AlgorithmChopda <vinaychopda4@gmail.com>
2024-02-27 15:01:19 +0530

    feat: added output to all cases

--- Assignment5/question1.js
+++ Assignment5/question1.js
@@ -1,7 +1,7 @@
 /*
     1. What is the difference between ++i and i++?
         - ++i will increment value of i first and will then evaluate expression
-        - ++i will evaluate expression and will then increment the value of i
+        - i++ will evaluate expression and will then increment the value of i
     
     
     2. What do you think would happen if you pass an index beyond the range of the string? Or if you pass a negative index? Try it out.
@@ -17,4 +17,43 @@
         - in invalid case it will raise an exception i.e Expected char(any) at position any
             and other parts of code will not execute properly
 
+*/
+
+/*
+    JSON.stringify
+*/
+
+const user = {
+  subjects: ["C++", "Java"],
+  list: {
+    name: "abc",
+    age: 20,
+  },
+  val: "value",
+  marks: null,
+  temp: undefined,
+  a: "100",
+  isValid: true,
+  age: 50,
+};
+
+console.log("JSON:", JSON.stringify(user));
+
+/*
+    Preetier Output:
+    output: 
+    JSON: {
+        subjects: ["C++", "Java"],
+        list: { name: "abc", age: 20 },
+        val: "value",
+        marks: null,
+        a: "100",
+        isValid: true,
+        age: 50,
+    };
+*/
+
+/*
+    Actual Output:
+    JSON: {"subjects":["C++","Java"],"list":{"name":"abc","age":20},"val":"value","marks":null,"a":"100","isValid":true,"age":50}
 */
