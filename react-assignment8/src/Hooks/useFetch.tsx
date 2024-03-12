8539d55018f28e5f0b8040a7b11b6db721b0cddf
AlgorithmChopda <vinaychopda4@gmail.com>
2024-03-12 18:57:24 +0530

    feat[react-assignment8]: formik

---
+++ react-assignment8/src/Hooks/useFetch.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import toast from "react-hot-toast";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const result = await axios.get(url);
+        setData(result.data);
+      } catch {
+        toast.error("error while loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading };
+};
+
+export default useFetch;
