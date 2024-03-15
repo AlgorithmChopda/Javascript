f09875d488e80b278cc33b2fc5598befab481b70
AlgorithmChopda <vinaychopda4@gmail.com>
2024-03-15 15:51:13 +0530

    feat[react-assignment10]: added middleware

--- react-assignment10/src/redux/store.ts
+++ react-assignment10/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counterSlice'
+import logger from "redux-logger"
 
 export const store = configureStore({
     reducer: {
-        counter: counterSlice
+        counter: counterSlice,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   })
   
   export type RootState = ReturnType<typeof store.getState>
